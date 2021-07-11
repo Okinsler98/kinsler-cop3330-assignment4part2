886c1188bce456a7136607c37a2397a67794f38b
Orion Kinsler <okinsler98@gmail.com>
2021-07-11 19:01:19 -0400

    ListItem

--- src/main/java/ucf/assignments/ListItem.java
+++ src/main/java/ucf/assignments/ListItem.java
@@ -8,18 +8,24 @@ public class ListItem {
 
     public void updateName(String newName){
         //Change name to new name
+        name = newName;
     }
 
     public void updateDescription(String newDescription){
         //Change description to new descirption
+        description = newDescription;
     }
 
     public void updateDueDate(String newDate){
         //Change date to new date
+        dueDate = newDate;
     }
 
     public String toString(){
-        //Return name, description, and due date
+        String output = new String();
+        //Set output to name, description, and due date
+        output = "||" + name + "||" + description + "||" + dueDate;
+        //Return output
         return "String";
     }
 }
